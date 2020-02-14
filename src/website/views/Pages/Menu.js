c22ad6297ea117e34bc0eb37142b6f7c7f625664
Nataliia Hachok <norbinatali@gmail.com>
2020-02-14 18:27:59 +0200

    Update Menu.js

--- src/website/views/Pages/Menu.js
+++ src/website/views/Pages/Menu.js
@@ -1,35 +1,13 @@
-import React from "react";
-// @material-ui/core components
+mport React from "react";
 
-import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
-import Search from "@material-ui/icons/Search";
-import Email from "@material-ui/icons/Email";
-import Face from "@material-ui/icons/Face";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import Explore from "@material-ui/icons/Explore";
-// core components
-import CustomInput from "../Components/CustomInput/CustomInput.js";
-
-import Button from "../Components/CustomButtons/Button.js";
-import GridContainer from "../Components/Grid/GridContainer";
-import GridItem from "../Components/Grid/GridItem";
-
-import Header from "../Components/Header/Header.js";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import CustomDropdown from "../Components/CustomDropdown/CustomDropdown.js";
 
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
 
-import '../../asset/style.css';
 import styles from "../../asset/jss/views/components.js";
 
+import '../../asset/style.css';
 const useStyles = makeStyles(theme => ({
     ...styles,
 
@@ -57,27 +35,27 @@ function Menu() {
     const classes = useStyles();
     return(
         <div className={classes.section}>
-            <GridList cellHeight={380} className={classes.gridList} cols={3}>
-                <GridListTile  cols={1 || 1}>
+            <GridList cellHeight={300} className={classes.gridList} cols={3}>
+                <GridListTile  cols={1 || 2}>
                     <div className="parent" onClick="">
                         <div className="child bg-one">
 
                         </div>
                     </div>
                 </GridListTile>
-                <GridListTile  cols={1 || 1}>
+                <GridListTile  cols={1 || 2}>
                     <div className="parent" onClick="">
                         <div className="child bg-two">
 
                         </div>
                     </div></GridListTile>
-                <GridListTile  cols={1 || 1}>
+                <GridListTile  cols={1 || 2}>
                     <div className="parent" onClick="">
                         <div className="child bg-three">
 
                         </div>
                     </div></GridListTile>
-                <GridListTile  cols={1 || 1}>
+                <GridListTile  cols={1 || 2}>
                     <div className="parent" onClick="">
                         <div className="child bg-four">
 
