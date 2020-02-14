cbe2d031b2c7e222646d145143b847bb700cdac2
Nataliia Hachok <norbinatali@gmail.com>
2020-02-14 10:47:41 +0200

    Update Header.js

--- src/website/views/Components/Header/Header.js
+++ src/website/views/Components/Header/Header.js
@@ -11,6 +11,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import Hidden from "@material-ui/core/Hidden";
 import Drawer from "@material-ui/core/Drawer";
+import ButtonBase from '@material-ui/core/ButtonBase';
 // @material-ui/icons
 import Menu from "@material-ui/core/Menu";
 import MenuIcon from "@material-ui/icons/Menu";
