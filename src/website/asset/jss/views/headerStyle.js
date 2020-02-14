1d9c62aa2a818825c329e1781d956ab3fbff3882
Nataliia Hachok <norbinatali@gmail.com>
2020-02-14 10:21:01 +0200

    Update headerStyle.js

--- src/website/asset/jss/views/headerStyle.js
+++ src/website/asset/jss/views/headerStyle.js
@@ -11,7 +11,7 @@ import {
     boxShadow,
     drawerWidth
 } from "./material-kit.js";
-
+import image from "../../asset/img/75398288_2651122521574678_1167449025327136768_o.jpg"
 const headerStyle = {
     appBar: {
         display: "flex",
@@ -62,6 +62,7 @@ const headerStyle = {
         color: "inherit",
         padding: "8px 16px",
         letterSpacing: "unset",
+        backgroundImage:image,
         "&:hover,&:focus": {
             color: "inherit",
             background: "transparent"
