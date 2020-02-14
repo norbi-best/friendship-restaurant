8fb7ecf638d0fc7b8a34c7bc666c820ce81088a8
Nataliia Hachok <norbinatali@gmail.com>
2020-02-14 10:06:52 +0200

    Update Header.js

--- src/website/views/Components/Header/Header.js
+++ src/website/views/Components/Header/Header.js
@@ -141,7 +141,7 @@ Header.propTypes = {
     ]),
     rightLinks: PropTypes.node,
     leftLinks: PropTypes.node,
-    brand: PropTypes.string,
+    brand: PropTypes.node,
     fixed: PropTypes.bool,
     absolute: PropTypes.bool,
     // this will cause the sidebar to change the color from
